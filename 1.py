d0fb8d5dab35bd899086b323f1e365708fc0b737
Fazeel-Sagar <fazeelattari1212@gmail.com>
2025-02-22 18:15:41 +0500

    commit message

---
+++ 1.py
@@ -0,0 +1,19 @@
+def addition (farzaam_salary,shakeel_salary):
+    sum = farzaam_salary + shakeel_salary
+    print(sum)
+addition(50000,45000)    
+
+def substraction (farzaam_salary,shakeel_salary):
+    sub = farzaam_salary - shakeel_salary
+    print(sub)
+substraction(50000,45000) 
+
+def multiplication (farzaam_salary,shakeel_salary):
+    sub = farzaam_salary * shakeel_salary
+    print(sub)
+multiplication(50000,45000)    
+
+def Division (farzaam_salary,shakeel_salary):
+    sub = farzaam_salary % shakeel_salary
+    print(sub)
+Division(50000,45000)    
